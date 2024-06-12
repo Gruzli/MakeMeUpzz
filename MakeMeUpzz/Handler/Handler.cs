1ddc132a8d94cce25cab2e797b9816bd3cb7fac8
Gregorius <forvalem3@gmail.com>
2024-06-12 22:30:45 +0700

    Update Reopsitory

--- MakeMeUpzz/Handler/Handler.cs
+++ MakeMeUpzz/Handler/Handler.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using MakeMeUpzz.Model;
+using MakeMeUpzz.Reposiitory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
-namespace MakeMeUpzz.Handler
+namespace MakeMeUpzz.Handlers
 {
-    public class Handler
+    public class UserHandler
     {
+        private readonly UserRepository _userRepository;
+
+        public UserHandler()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public int GenerateUserId()
+        {
+            return _userRepository.generateId();
+        }
+
+        public void RegisterUser(User user)
+        {
+            _userRepository.InputRegisterUser(user);
+        }
+
+        public bool AuthenticateUser(string username, string password)
+        {
+            return _userRepository.Authentication(username, password);
+        }
     }
 }
