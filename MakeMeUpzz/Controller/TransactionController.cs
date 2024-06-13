1f0b427f82cb0721ec950265ad107c96c92e8213
Gregorius <forvalem3@gmail.com>
2024-06-13 21:28:04 +0700

    Dataset

---
+++ MakeMeUpzz/Controller/TransactionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MakeMeUpzz.Controller
+{
+    public class TransactionController
+    {
+        public static String checkTransaction(int total)
+        {
+            String response = "";
+            if (total <= 0)
+            {
+                response = "Total must be more than 0!";
+            }
+
+            return response;
+        }
+    }
+}
