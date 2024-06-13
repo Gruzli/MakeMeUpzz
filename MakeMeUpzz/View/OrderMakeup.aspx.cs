5b40c99e41f085b8ad223675c65d7d3d9043f17e
ValeskaLim <valeskavalentinekklesia@gmail.com>
2024-06-13 22:06:15 +0700

    Added profile

---
+++ MakeMeUpzz/View/OrderMakeup.aspx.cs
@@ -0,0 +1,31 @@
+﻿using MakeMeUpzz.Model;
+using MakeMeUpzz.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MakeMeUpzz.View
+{
+    
+    public partial class OrderMakeup : System.Web.UI.Page
+    {
+        MakeupRepository makeupRepo = new MakeupRepository();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GridViewOrder.DataSource = makeupRepo.GetAllMakeups();
+                GridViewOrder.DataBind();
+            }
+        }
+
+        protected void GridViewOrder_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            
+        }
+    }
+}
